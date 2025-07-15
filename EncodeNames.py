1d2be8a936466d556d58ab75cf1bf449b26aad9f
VictorPlaysBadminton <162258396+VictorPlaysBadminton@users.noreply.github.com>
2025-07-15 17:20:54 +1200

    finished first prototype of code, still need to fix up BIP

--- EncodeNames.py
+++ EncodeNames.py
@@ -1,5 +1,89 @@
 import numpy as np
 import pandas as pd
-import sklearn as sk
+from sklearn.preprocessing import LabelEncoder
 import pulp
 
+
+def get_data(fileID):
+    """
+    This function gets the data which we are trying to encode.
+    :param fileID: Name of file which we are reading from as a string
+    :return df: Dataframe which contains the data
+    """
+    df = pd.read_csv(fileID)
+    return df
+
+
+def clean_data(df, labels):
+    """
+    This function cleans the data which we are trying to encode.
+    :param df: the data which we are trying to clean
+    :param labels: the labels which we are trying use
+    :return clean_df: Dataframe which contains the cleaned data
+    """
+    clean_df = df.loc[:, labels]
+    clean_df = clean_df.dropna(axis = 0)
+
+    return clean_df
+
+
+def encode_names(df, labels, skill, gender, club):
+    """
+    This function encodes the labels with values.
+    :param df: the data which we are trying to encode
+    :param labels: the labels which we are trying use
+    :param skill: the skill which we are trying to encode
+    :param gender: the gender which we are trying to encode
+    :param club: the club which we are trying to encode
+    :return: encoded_df: Dataframe which contains the encoded data
+    """
+    # Create encoders
+    skillencode = LabelEncoder()
+    clubencode = LabelEncoder()
+    genderencode = LabelEncoder()
+
+    # Fit data to encoder
+    skillencode.fit(skill)
+    clubencode.fit(club)
+    genderencode.fit(gender)
+    genderencode.classes_ = np.array(gender)
+    clubencode.classes_ = np.array(club)
+    skillencode.classes_ = np.array(skill)
+
+    # create copy of original data
+    encoded_data = df.copy()
+    for label in labels:
+        if "Skill" in label:
+            encoded_data[label] = skillencode.transform(encoded_data[label])
+        if "Gender" in label:
+            encoded_data[label] = genderencode.transform(encoded_data[label])
+        if "Club" in label:
+            encoded_data[label] = clubencode.transform(encoded_data[label])
+
+    return encoded_data
+
+def simply_gender(df,gender):
+    """
+    This function simplifies the gender label into a binary values.
+    :param df: the dataframe we are editing
+    :param gender: the gender label
+    :return:
+    """
+    for index in df.loc[gender, :].index:
+        if df.loc[gender, index] == 2:
+            df.loc[gender, index] = 1
+    return df
+
+
+def remove_substring_from_df(df: pd.DataFrame, substring: str) -> pd.DataFrame:
+    """
+    Remove a specific substring from all string values in a DataFrame.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame.
+        substring (str): The substring to remove.
+
+    Returns:
+        pd.DataFrame: A new DataFrame with the substring removed from string values.
+    """
+    return df.applymap(lambda x: x.replace(substring, '') if isinstance(x, str) else x)
---
+++ Generator.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from pulp import *
+from EncodeNames import *
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+# Import Data
+raw_data = get_data('C:/Users/victo/OneDrive/Desktop/Group Gen/Test1.csv')
+raw_data_labels = raw_data.columns
+
+
+# combine first and last names
+df = raw_data.copy()
+df["Full_Name"] = df["First Name "] + ' ' + df["Last Name "]
+
+# preprocess and clean data
+labels = ["Full_Name", "Gender ", "Badminton Skill Level ", "Which Club are you a member of?"]
+preprocessed_data = clean_data(df, labels)
+
+
+# Encode the data
+skill = ["Beginner ( This is your first time playing badminton, or you don't have much experience)", "Intermediate ( Solid grasp of techniques , got some experience under your belt)", "Advanced (Very confident in skills, likely participated in tournaments or leagues before)"]
+gender = ["Male", "Female", "Prefer not to say"]
+club = ["ESA", "UABC", "Non-Member"]
+encoded_data = encode_names(preprocessed_data ,labels, skill, gender, club)
+print(encoded_data.head())
+
+# Transpose data to create Binary Integer Program
+encoded_data_lp = encoded_data.transpose(copy=True)
+encoded_data_lp = encoded_data_lp.rename(columns=encoded_data_lp.iloc[0])
+encoded_data_lp = encoded_data_lp.drop(encoded_data_lp.index[0])
+encoded_data_lp = simply_gender(encoded_data_lp,"Gender ")
+print(encoded_data_lp)
+
+
+
+# Groups Dataframe
+groups ={}
+group = []
+
+# Create BIP dictionary
+people = {}
+cost = {}
+for i in encoded_data["Full_Name"]:
+    people.update({i:0})
+    cost.update({i:1})
+
+print(cost)
+
+
+# BIP
+prob = LpProblem("UABC_Social_Event_Problem", LpMinimize)
+variables = LpVariable.dicts("People", people,0,1,LpInteger)
+
+
+# Minimise
+prob += lpSum([cost[index] * variables[index] for index in variables])
+
+# Subject to:
+prob += lpSum([variables[index] for index in variables]) <= 10 # Number of people per group
+prob += lpSum([variables[index] for index in variables]) >= 10
+
+prob += lpSum([encoded_data_lp.loc["Gender ", index] * variables[index] for index in variables]) >= 2 # More than 2 girls per group
+
+prob += lpSum([encoded_data_lp.loc["Badminton Skill Level ", index] * variables[index] for index in variables]) <= 30 # Limit number of advance players per group
+
+prob += lpSum([encoded_data_lp.loc["Which Club are you a member of?", index] * variables[index] for index in variables]) <= 30 # Even number of club members per group
+for j in range(1,7):
+    # Solve problem
+    prob.writeLP('BIP_Social_Event_Problem.lp')
+    prob.solve()
+
+    for v in prob.variables():
+        if v.varValue == 1:
+            print(v.name)
+            group.append(v.name)
+            prob += v == 0
+    groups.update({j:group})
+    group = []
+
+df = pd.DataFrame.from_dict(groups, orient='index')
+df = df.transpose()
+df = remove_substring_from_df(df,"People_")
+print(df)
